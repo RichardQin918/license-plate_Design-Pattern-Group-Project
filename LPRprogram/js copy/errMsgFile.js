66c70b621c6ff1c56e5af5c2db41cdae39a062ac
sherzi <shermeen.kazi231@gmail.com>
2020-02-03 20:57:26 -0500

    first commit

---
+++ LPRprogram/js copy/errMsgFile.js
@@ -0,0 +1,13 @@
+// Try/Catch error handling
+try {
+alert("Hello User 9_events");
+console.log("ok");
+}
+catch(err) {
+document.getElementById("msgBox").innerHTML = err.message; }
+finally {
+// If needed, you can do something here regardless of what happens in try/catch
+}
+
+
+
